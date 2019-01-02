8bbe0099c0547cc34ce6e2745df8ee65f090db33
Phong Nguyen <nhphong1406@gmail.com>
2019-01-03 00:43:51 +0700

    Changes TAPPING_TERM to 200 and Remove PLACEHOLDER

--- config.h
+++ config.h
@@ -42,6 +42,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MOUSEKEY_MAX_SPEED   5
 #define MOUSEKEY_WHEEL_DELAY 0
 
-#define TAPPING_TERM    150
+#define TAPPING_TERM    200
 #define IGNORE_MOD_TAP_INTERRUPT // this makes it possible to do rolling combos (zx) with keys that convert to other keys on hold (z becomes ctrl when you hold it, and when this option isn't enabled, z rapidly followed by x actually sends Ctrl-x. That's bad.)
 
