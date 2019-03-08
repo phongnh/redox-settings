1d0478a0732a170731d3948d02ebb490a5dcf19c
Phong Nguyen <nhphong1406@gmail.com>
2019-03-09 01:36:54 +0700

    Update rules.mk

--- rules.mk
+++ rules.mk
@@ -1,7 +1,4 @@
-BOOTMAGIC_ENABLE = no   # Virtual DIP switch configuration(+1000)
-MOUSEKEY_ENABLE  = yes  # Mouse keys(+4700)
-EXTRAKEY_ENABLE  = yes  # Audio control and System control(+450)
-CONSOLE_ENABLE   = no   # Console for debug(+400)
-COMMAND_ENABLE   = no   # Commands for debug and configuration
-NKRO_ENABLE      = yes  # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-RGBLIGHT_ENABLE  = yes  # Enable WS2812 RGB underlight.  Do not enable this with audio at the same time.
+BOOTLOADER      = caterina
+COMMAND_ENABLE  = no        # Commands for debug and configuration
+NKRO_ENABLE     = yes       # USB Nkey Rollover
+RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
